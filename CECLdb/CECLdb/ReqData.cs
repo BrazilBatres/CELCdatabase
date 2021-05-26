dcecd5ea2249066a7f2c31f898413fa8c092ce6c
BrazilBatres <60472938+BrazilBatres@users.noreply.github.com>
2021-05-25 21:27:48 -0600

    Solución

---
+++ CECLdb/CECLdb/ReqData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CECLdb
+{
+    public partial class ReqData : Form
+    {
+        public ReqData()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CECLdb/CECLdb/ReqData2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CECLdb
+{
+    public partial class ReqData2 : Form
+    {
+        public ReqData2()
+        {
+            InitializeComponent();
+        }
+    }
+}
