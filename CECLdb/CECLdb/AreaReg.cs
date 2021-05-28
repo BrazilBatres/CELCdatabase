59d42961a7569a30cfd5e4533317e79bf834a76f
Jocelynardon <ozelynardon@gmail.com>
2021-05-28 11:16:34 -0600

    Interfaces de Registro
    Poder agregar los datos de cada área

---
+++ CECLdb/CECLdb/AreaReg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CECLdb
+{
+    public partial class AreaReg : Form
+    {
+        public AreaReg()
+        {
+            InitializeComponent();
+        }
+
+        private void lblAreaName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
